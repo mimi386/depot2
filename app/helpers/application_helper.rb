dd0720f820322fdf4679f6af0559c803bed920be
mimi386 <johnwalk55@yahoo.com>
2016-07-18 08:23:46 +0200

    initial commit'

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,20 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
+  def currency_to_locale(price)
+    price = price * 0.879815 if 'es' == I18n.locale.to_s
+    number_to_currency price
+  end
+end
