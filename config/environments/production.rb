f2cb55ed80b31475cef893cacd79989e59e0a1f6
mimi386 <johnwalk55@yahoo.com>
2016-10-01 00:37:06 +0200

    update70

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,13 +19,14 @@ Depot::Application.configure do
  # config.lograge.enabled = true
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+  # config.paths.add "config/locales", eager_load: true
+  config.eager_load_paths += %W(#{config.root}/config/locales)
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
